2b062c287a00d7840fa554f94cb7822528cb326c
noko <254495579a@gmail.com>
2017-11-17 16:34:38 -0500

    q4
    q4

--- core/src/main/java/jenkins/model/IdStrategy.java
+++ core/src/main/java/jenkins/model/IdStrategy.java
@@ -246,30 +246,23 @@ public abstract class IdStrategy extends AbstractDescribableImpl<IdStrategy> imp
                         }
                     } else if (c == '$') {
                         StringBuilder hex = new StringBuilder(4);
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
+                       
+                        
+                        boolean breakLoop= false;
+                        for(int j=0;j<4;j++)
+                        {
+                        	i++;
+                        	if(i<chars.length) {
+                        		hex.append(chars[i]);
+                        	}
+                        	else {breakLoop = true;
+                        			break;}
+                        	
                         }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
+                        if( breakLoop )
+                        	break;
+                        
+                        
                         buf.append(Character.valueOf((char)Integer.parseInt(hex.toString(), 16)));
                     }
                 }
